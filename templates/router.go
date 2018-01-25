f24ed9399151b62a66fcb249a8163a5fa5c699d0
Tim Iskhakov <timiskhakov@gmail.com>
2018-01-25 15:14:38 +0200

    Remove Pragma header for HTTP 1.0

--- templates/router.go
+++ templates/router.go
@@ -233,7 +233,6 @@ func respondJSON(w http.ResponseWriter, data interface{}, dataType string, statu
 
 	// prevent caching
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1.
-	w.Header().Set("Pragma", "no-cache") // HTTP 1.0.
 	w.Header().Set("Expires", "0") // Proxies.
 
 	w.Header().Set("Content-Type", "application/json")
