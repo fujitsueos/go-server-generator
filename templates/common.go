fae1360c955f8404a0f7a6992ff81255f637cd57
Vincent van der Weele <vincentvanderweele@hotmail.com>
2018-07-23 16:03:19 +0300

    Prevent empty else blocks

--- templates/common.go
+++ templates/common.go
@@ -1,13 +1,14 @@
 package templates
 
 import (
+	"bytes"
 	"errors"
 	"strings"
 	"text/template"
 )
 
-func parse(name, tpl string) *template.Template {
-	return template.Must(template.New(name).Funcs(template.FuncMap{
+func parse(name, tpl string) (t *template.Template) {
+	t = template.Must(template.New(name).Funcs(template.FuncMap{
 		// allow on-the-fly maps in templates; see https://stackoverflow.com/a/18276968/2095090
 		"dict": func(values ...interface{}) (dict map[string]interface{}, err error) {
 			if len(values)%2 != 0 {
@@ -25,6 +26,22 @@ func parse(name, tpl string) *template.Template {
 			}
 			return
 		},
+		"templateAsString": func(name string, pipelines ...interface{}) (string, error) {
+			if len(pipelines) > 1 {
+				return "", errors.New("Template takes max one parameter")
+			}
+
+			var pipeline interface{}
+			if len(pipelines) == 1 {
+				pipeline = pipelines[0]
+			}
+
+			buffer := &bytes.Buffer{}
+			err := t.ExecuteTemplate(buffer, name, pipeline)
+			return buffer.String(), err
+		},
 		// replace ' by `: we never need ' and we are not allowed to use ` in templates...
 	}).Parse(strings.Replace(tpl, "'", "`", -1)))
+
+	return
 }
